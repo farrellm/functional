74f07c30bc6c749d6de94c99cb732337ff2c3a86
Matthew Farrell <farrellm23@gmail.com>
2014-09-26 18:42:29 -0400

    rename project

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject functional "0.1.0-SNAPSHOT"
+(defproject kant "0.1.0-SNAPSHOT"
   :description "Monads for Clojure"
-  :url "https://github.com/farrellm/functional"
+  :url "https://github.com/farrellm/kant"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
