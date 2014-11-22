578b2f8fc470ed5d5c18620bf064ea37a2e7f176
Matthew Farrell <farrellm@alum.mit.edu>
2014-11-22 15:20:13 -0500

    IFn is too general, breaks sequences

--- src/kant/arrow/function.clj
+++ src/kant/arrow/function.clj
@@ -1,7 +1,7 @@
 (ns kant.arrow.function
   (:require [kant.monad.protocol :refer :all]))
 
-(extend-type clojure.lang.IFn
+(extend-type clojure.lang.AFunction
   Category
   (-id [_] identity)
   (-comp [a b] (comp a b))
