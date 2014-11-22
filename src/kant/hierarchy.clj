819da92b78cdc166d409ac9d4f291187e893beb7
Matthew Farrell <farrellm@alum.mit.edu>
2014-11-22 02:10:40 -0500

    clean up hierarchy

--- src/kant/hierarchy.clj
+++ src/kant/hierarchy.clj
@@ -3,44 +3,37 @@
 
 ;;   functor
 ;;      |
-;; applicative      category
-;;      |     \     /
-;;    monad    arrow
-;;        \      +--------------+
-;;          \    |              |
-;;          arrow-apply    arrow-choice
+;; applicative        category
+;;      |    \----\   /
+;;      |         arrow
+;;      |           |
+;;      |      arrow-choice
+;;      |           |
+;;    monad -- arrow-apply
 
 (derive ::applicative ::functor)
-
 (derive ::monad ::applicative)
 
 (derive ::arrow ::category)
+(derive ::arrow-choice ::arrow)
+(derive ::arrow-apply ::arrow-choice)
+
 (derive ::arrow ::applicative)
 
-(derive ::arrow-choice ::arrow)
-
-(derive ::arrow-apply ::arrow)
-(derive ::arrow-apply ::monad)
-
-(def monad        (cons [::monad        #'kant.monad.protocol/Monad]       []))
 (def arrow-apply  (cons [::arrow-apply  #'kant.monad.protocol/ArrowApply]  []))
-
-(def arrow-choice (cons [::arrow-choice #'kant.monad.protocol/ArrowChoice] []))
-
-(def arrow        (cons [::arrow        #'kant.monad.protocol/Arrow]       (concat arrow-apply
-                                                                                   arrow-choice)))
-
-(def applicative  (cons [::applicative  #'kant.monad.protocol/Applicative] (concat monad
-                                                                                   arrow)))
-(def functor      (cons [::functor      #'kant.monad.protocol/Functor]     applicative))
-
+(def arrow-choice (cons [::arrow-choice #'kant.monad.protocol/ArrowChoice] arrow-apply))
+(def arrow        (cons [::arrow        #'kant.monad.protocol/Arrow]       arrow-choice))
 (def category     (cons [::category     #'kant.monad.protocol/Category]    arrow))
 
-(def heirarchy (hash-map :monad monad
+(def monad        (cons [::monad        #'kant.monad.protocol/Monad]       []))
+(def applicative  (cons [::applicative  #'kant.monad.protocol/Applicative] (concat monad arrow)))
+(def functor      (cons [::functor      #'kant.monad.protocol/Functor]     applicative))
+
+(def heirarchy (hash-map :arrow-apply arrow-apply
                          :arrow-choice arrow-choice
-                         :arrow-apply arrow-apply
                          :arrow arrow
                          :category category
+                         :monad monad
                          :applicative applicative
                          :functor functor))
 
